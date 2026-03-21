5dab73cdd662fdfffa29361b6d0e5270402dcfad
thezarababy <jyokoduwa@gmail.com>
2026-03-21 18:34:04 +0100

    feat: select role

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 
+import SelectRole from "@/components/SelectRole";
 import Signup from "./signup/page";
 
 
 export default function Home() {
   return (
     <div className="auth">
-      <Signup />
+      {/* <Signup /> */}
+      <SelectRole />
     </div>
   );
 }
