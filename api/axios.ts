255ed2430f6bf79f38393c2b10ecb05d27ebb3b0
thezarababy <jyokoduwa@gmail.com>
2026-08-29 22:01:50 +0100

    fix: Update baseURL in axios configuration to use production API endpoint

--- api/axios.ts
+++ api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromCookie, removeTokenCookie } from "../utils/token";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api-gateway-3344.onrender.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
